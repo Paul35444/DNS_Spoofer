091d141c2b3a25d5a7d422125209a048a49ed795
Paul St. Amour <paul35444@aol.com>
2021-03-16 20:08:08 -0500

    -created instance of queue -bind queue to queue num 0 and callback to process_packet func -run queue

--- DNS_Spoofer.py
+++ DNS_Spoofer.py
@@ -2,4 +2,8 @@
 
 import netfilterqueue
 
-
+#create instace of queue 
+queue = netfilterqueue.NetfilterQueue()
+#bind queue to queue number 0 and callback to func process_packet
+queue.bind(0, process_packet)
+queue.run
