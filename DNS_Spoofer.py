58aab19e9891c24c81901ffa72726a0e06f4fea7
Paul St. Amour <paul35444@aol.com>
2021-03-17 10:52:15 -0500

    -import scapy lib

--- DNS_Spoofer.py
+++ DNS_Spoofer.py
@@ -1,6 +1,7 @@
 #!/usr.bin/env python
 
 import netfilterqueue
+import scapy.all as scapy
 
 def process_packet():
     print(packet)
@@ -12,3 +13,4 @@ queue = netfilterqueue.NetfilterQueue()
 #bind queue to queue number 0 and callback to func process_packet
 queue.bind(0, process_packet)
 queue.run
+
